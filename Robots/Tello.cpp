b27906ada8172ca4b5270cd499d2e2a45c1c1797
nicholasadr <adr.nicholas@gmail.com>
2023-07-03 13:18:23 -0400

    Add Tello's second leg

--- Robots/Tello.cpp
+++ Robots/Tello.cpp
@@ -10,98 +10,155 @@ namespace grbda
 	// Set gravity in z direction
 	model.setGravity(Vec3<double>{0., 0., grav});
 
-	// Hip clamp rotor
-	const std::string hip_clamp_rotor_name = "hip-clamp-rotor";
-	const std::string hip_clamp_rotor_parent_name = "ground";
-	const SpatialTransform hip_clamp_rotor_Xtree = SpatialTransform(R_hip_clamp, p_hip_clamp);
-	const SpatialInertia<double> hip_clamp_rotor_spatial_inertia = SpatialInertia<double>{hip_clamp_rotor_mass, hip_clamp_rotor_CoM, hip_clamp_rotor_inertia};
-	auto hip_clamp_rotor = model.registerBody(hip_clamp_rotor_name, hip_clamp_rotor_spatial_inertia, hip_clamp_rotor_parent_name, hip_clamp_rotor_Xtree);
-
-	// Hip clamp
-	const std::string hip_clamp_name = "hip-clamp";
+	std::vector<std::string> sides = {"left","right"};
 	const std::string hip_clamp_parent_name = "ground";
-	const SpatialTransform hip_clamp_Xtree = SpatialTransform(R_hip_clamp_rotor, p_hip_clamp_rotor);
-	const SpatialInertia<double> hip_clamp_spatial_inertia = SpatialInertia<double>{hip_clamp_mass, hip_clamp_CoM, hip_clamp_inertia};
-	auto hip_clamp = model.registerBody(hip_clamp_name, hip_clamp_spatial_inertia, hip_clamp_parent_name, hip_clamp_Xtree);
+	const std::string hip_clamp_rotor_parent_name = "ground";
 
-	// Body Cluster 1
-	const std::string body_cluster_name_1 = "hip-clamp";
-	const int gear_ratio = 1;
-	auto joint_1 = std::make_shared<GeneralizedJoints::RevoluteWithRotor>(
+	for (size_t i(0); i < 2; i++)
+	{
+	    const std::string side = sides[i];
+
+	    // Hip clamp rotor
+	    const SpatialTransform hip_clamp_rotor_Xtree = i == 0 ? SpatialTransform(R_left_hip_clamp_rotor,
+										     p_left_hip_clamp_rotor)
+								  : SpatialTransform(R_right_hip_clamp_rotor,
+										     p_right_hip_clamp_rotor);
+	    const std::string hip_clamp_rotor_name = side + "-hip-clamp-rotor";
+	    const SpatialInertia<double> hip_clamp_rotor_spatial_inertia = SpatialInertia<double>{
+		hip_clamp_rotor_mass, hip_clamp_rotor_CoM, hip_clamp_rotor_inertia};
+	    auto hip_clamp_rotor = model.registerBody(hip_clamp_rotor_name, hip_clamp_rotor_spatial_inertia,
+						      hip_clamp_rotor_parent_name, hip_clamp_rotor_Xtree);
+
+	    // Hip clamp
+	    const SpatialTransform hip_clamp_Xtree = i == 0 ? SpatialTransform(R_left_hip_clamp,
+									       p_left_hip_clamp)
+							    : SpatialTransform(R_right_hip_clamp,
+									       p_right_hip_clamp);
+	    const std::string hip_clamp_name = side + "-hip-clamp";
+	    const SpatialInertia<double> hip_clamp_spatial_inertia = SpatialInertia<double>{hip_clamp_mass,
+		hip_clamp_CoM, hip_clamp_inertia};
+	    auto hip_clamp = model.registerBody(hip_clamp_name, hip_clamp_spatial_inertia,
+						hip_clamp_parent_name, hip_clamp_Xtree);
+
+	    // Hip clamp cluster
+	    const std::string hip_clamp_cluster_name = side + "-hip-clamp";
+	    const int gear_ratio = 1;
+	    auto hip_clamp_generalized_joint = std::make_shared<GeneralizedJoints::RevoluteWithRotor>(
 		hip_clamp_rotor, hip_clamp, CoordinateAxis::Z, CoordinateAxis::Z, gear_ratio);
-	model.appendRegisteredBodiesAsCluster(body_cluster_name_1, joint_1);
+	    model.appendRegisteredBodiesAsCluster(hip_clamp_cluster_name, hip_clamp_generalized_joint);
 
-	// Hip differential left rotor
-	const std::string hip_rotor_left_name = "hip-left-rotor";
-	const std::string hip_rotor_left_parent_name = "hip-clamp";
-	const SpatialTransform hip_rotor_left_Xtree = SpatialTransform(R_hip_rotor_left, p_hip_rotor_left);
-	const SpatialInertia<double> hip_rotor_left_spatial_inertia = SpatialInertia<double>{hip_rotor_left_mass, hip_rotor_left_CoM, hip_rotor_left_inertia};
-	auto hip_rotor_left = model.registerBody(hip_rotor_left_name, hip_rotor_left_spatial_inertia, hip_rotor_left_parent_name, hip_rotor_left_Xtree);
-	
-	// Hip differential right rotor
-	const std::string hip_rotor_right_name = "hip-right-rotor";
-	const std::string hip_rotor_right_parent_name = "hip-clamp";
-	const SpatialTransform hip_rotor_right_Xtree = SpatialTransform(R_hip_rotor_right, p_hip_rotor_right);
-	const SpatialInertia<double> hip_rotor_right_spatial_inertia = SpatialInertia<double>{hip_rotor_right_mass, hip_rotor_right_CoM, hip_rotor_right_inertia};
-	auto hip_rotor_right = model.registerBody(hip_rotor_right_name, hip_rotor_right_spatial_inertia, hip_rotor_right_parent_name, hip_rotor_right_Xtree);
+	    // Hip differential rotor 1
+	    const SpatialTransform hip_rotor_1_Xtree = i == 0 ? SpatialTransform(R_left_hip_rotor_1,
+										 p_left_hip_rotor_1)
+							      : SpatialTransform(R_right_hip_rotor_1,
+										 p_right_hip_rotor_1);
+	    const std::string hip_rotor_1_name = side + "-hip-rotor-1";
+	    const std::string hip_rotor_1_parent_name = side + "-hip-clamp";
+	    const SpatialInertia<double> hip_rotor_1_spatial_inertia = SpatialInertia<double>{hip_rotor_1_mass,
+		hip_rotor_1_CoM, hip_rotor_1_inertia};
+	    auto hip_rotor_1 = model.registerBody(hip_rotor_1_name, hip_rotor_1_spatial_inertia,
+						  hip_rotor_1_parent_name, hip_rotor_1_Xtree);
 
-	// Gimbal
-	const std::string gimbal_name = "gimbal";
-	const std::string gimbal_parent_name = "hip-clamp";
-	const SpatialTransform gimbal_Xtree = SpatialTransform(R_gimbal, p_gimbal);
-	const SpatialInertia<double> gimbal_spatial_inertia = SpatialInertia<double>{gimbal_mass, gimbal_CoM, gimbal_inertia};
-	auto gimbal = model.registerBody(gimbal_name, gimbal_spatial_inertia, gimbal_parent_name, gimbal_Xtree);
+	    // Hip differential rotor 2
+	    const SpatialTransform hip_rotor_2_Xtree = i == 0 ? SpatialTransform(R_left_hip_rotor_2,
+										 p_left_hip_rotor_2)
+							      : SpatialTransform(R_right_hip_rotor_2,
+										 p_right_hip_rotor_2);
+	    const std::string hip_rotor_2_name = side + "-hip-rotor-2";
+	    const std::string hip_rotor_2_parent_name = side + "-hip-clamp";
+	    const SpatialInertia<double> hip_rotor_2_spatial_inertia = SpatialInertia<double>{hip_rotor_2_mass,
+		hip_rotor_2_CoM, hip_rotor_2_inertia};
+	    auto hip_rotor_2 = model.registerBody(hip_rotor_2_name, hip_rotor_2_spatial_inertia,
+						  hip_rotor_2_parent_name, hip_rotor_2_Xtree);
 
-	// Thigh
-	const std::string thigh_name = "thigh";
-	const std::string thigh_parent_name = "gimbal";
-	const SpatialTransform thigh_Xtree = SpatialTransform(R_thigh, p_thigh);
-	const SpatialInertia<double> thigh_spatial_inertia = SpatialInertia<double>{thigh_mass, thigh_CoM, thigh_inertia};
-	auto thigh = model.registerBody(thigh_name, thigh_spatial_inertia, thigh_parent_name, thigh_Xtree);
-	
-	// Body Cluster 2
-	const std::string body_cluster_name_2 = "hip-differential";
-	auto joint_2 = std::make_shared<GeneralizedJoints::TelloHipDifferential>(
-		hip_rotor_left, hip_rotor_right, gimbal, thigh,
-		CoordinateAxis::Z, CoordinateAxis::Z,
+	    // Gimbal
+	    const SpatialTransform gimbal_Xtree = i == 0 ? SpatialTransform(R_left_gimbal,
+									    p_left_gimbal)
+							 : SpatialTransform(R_right_gimbal,
+									    p_right_gimbal);
+	    const std::string gimbal_name = side + "-gimbal";
+	    const std::string gimbal_parent_name = side + "-hip-clamp";
+	    const SpatialInertia<double> gimbal_spatial_inertia = SpatialInertia<double>{gimbal_mass,
+		gimbal_CoM, gimbal_inertia};
+	    auto gimbal = model.registerBody(gimbal_name, gimbal_spatial_inertia, gimbal_parent_name,
+					     gimbal_Xtree);
+
+	    // Thigh
+	    const SpatialTransform thigh_Xtree = i == 0 ? SpatialTransform(R_left_thigh,
+									   p_left_thigh)
+							: SpatialTransform(R_right_thigh,
+									   p_right_thigh);
+	    const std::string thigh_name = side + "-thigh";
+	    const std::string thigh_parent_name = side + "-gimbal";
+	    const SpatialInertia<double> thigh_spatial_inertia = SpatialInertia<double>{thigh_mass,
+		thigh_CoM, thigh_inertia};
+	    auto thigh = model.registerBody(thigh_name, thigh_spatial_inertia, thigh_parent_name, thigh_Xtree);
+
+	    // Hip differential cluster
+	    const std::string hip_differential_cluster_name = side + "-hip-differential";
+	    auto hip_differential_generalized_joint = std::make_shared<GeneralizedJoints::TelloHipDifferential>(
+		hip_rotor_1, hip_rotor_2, gimbal, thigh, CoordinateAxis::Z, CoordinateAxis::Z,
 		CoordinateAxis::X, CoordinateAxis::Y);
-	model.appendRegisteredBodiesAsCluster(body_cluster_name_2, joint_2);
+	    model.appendRegisteredBodiesAsCluster(hip_differential_cluster_name,
+						  hip_differential_generalized_joint);
 
-	// Knee-ankle differential left rotor
-	const std::string knee_ankle_rotor_left_name = "knee-ankle-left-motor";
-	const std::string knee_ankle_rotor_left_parent_name = "thigh";
-	const SpatialTransform knee_ankle_rotor_left_Xtree = SpatialTransform(R_knee_ankle_rotor_left, p_knee_ankle_rotor_left);
-	const SpatialInertia<double> knee_ankle_rotor_left_spatial_inertia = SpatialInertia<double>{knee_ankle_rotor_left_mass, knee_ankle_rotor_left_CoM, knee_ankle_rotor_left_inertia};
-	auto knee_ankle_rotor_left = model.registerBody(knee_ankle_rotor_left_name, knee_ankle_rotor_left_spatial_inertia, knee_ankle_rotor_left_parent_name, knee_ankle_rotor_left_Xtree);
-	
-	// Knee-ankle differential right motor
-	const std::string knee_ankle_rotor_right_name = "knee-ankle-right-motor";
-	const std::string knee_ankle_rotor_right_parent_name = "thigh";
-	const SpatialTransform knee_ankle_rotor_right_Xtree = SpatialTransform(R_knee_ankle_rotor_right, p_knee_ankle_rotor_right);
-	const SpatialInertia<double> knee_ankle_rotor_right_spatial_inertia = SpatialInertia<double>{knee_ankle_rotor_right_mass, knee_ankle_rotor_right_CoM, knee_ankle_rotor_right_inertia};
-	auto knee_ankle_rotor_right = model.registerBody(knee_ankle_rotor_right_name, knee_ankle_rotor_right_spatial_inertia, knee_ankle_rotor_right_parent_name, knee_ankle_rotor_right_Xtree);
+	    // Knee-ankle differential rotor 1
+	    const SpatialTransform knee_ankle_rotor_1_Xtree = i == 0 ?
+		SpatialTransform(R_left_knee_ankle_rotor_1, p_left_knee_ankle_rotor_1) :
+		SpatialTransform(R_right_knee_ankle_rotor_1, p_right_knee_ankle_rotor_1);
+	    const std::string knee_ankle_rotor_1_name = side + "-knee-ankle-rotor-1";
+	    const std::string knee_ankle_rotor_1_parent_name = side + "-thigh";
+	    const SpatialInertia<double> knee_ankle_rotor_1_spatial_inertia = SpatialInertia<double>{
+		knee_ankle_rotor_1_mass, knee_ankle_rotor_1_CoM, knee_ankle_rotor_1_inertia};
+	    auto knee_ankle_rotor_1 = model.registerBody(knee_ankle_rotor_1_name,
+							 knee_ankle_rotor_1_spatial_inertia,
+							 knee_ankle_rotor_1_parent_name,
+							 knee_ankle_rotor_1_Xtree);
 
-	// Shin
-	const std::string shin_name = "shin";
-	const std::string shin_parent_name = "thigh";
-	const SpatialTransform shin_Xtree = SpatialTransform(R_shin, p_shin);
-	const SpatialInertia<double> shin_spatial_inertia = SpatialInertia<double>{shin_mass, shin_CoM, shin_inertia};
-	auto shin = model.registerBody(shin_name, shin_spatial_inertia, shin_parent_name, shin_Xtree);
+	    // Knee-ankle differential rotor 2
+	    const SpatialTransform knee_ankle_rotor_2_Xtree = i == 0 ?
+		SpatialTransform(R_left_knee_ankle_rotor_2, p_left_knee_ankle_rotor_2) :
+		SpatialTransform(R_right_knee_ankle_rotor_2, p_right_knee_ankle_rotor_2);
+	    const std::string knee_ankle_rotor_2_name = side + "-knee-ankle-rotor-2";
+	    const std::string knee_ankle_rotor_2_parent_name = side + "-thigh";
+	    const SpatialInertia<double> knee_ankle_rotor_2_spatial_inertia = SpatialInertia<double>{
+		knee_ankle_rotor_2_mass, knee_ankle_rotor_2_CoM, knee_ankle_rotor_2_inertia};
+	    auto knee_ankle_rotor_2 = model.registerBody(knee_ankle_rotor_2_name,
+							 knee_ankle_rotor_2_spatial_inertia,
+							 knee_ankle_rotor_2_parent_name,
+							 knee_ankle_rotor_2_Xtree);
 
-	// Foot
-	const std::string foot_name = "foot";
-	const std::string foot_parent_name = "shin";
-	const SpatialTransform foot_Xtree = SpatialTransform(R_foot, p_foot);
-	const SpatialInertia<double> foot_spatial_inertia = SpatialInertia<double>{foot_mass, foot_CoM, foot_inertia};
-	auto foot = model.registerBody(foot_name, foot_spatial_inertia, foot_parent_name, foot_Xtree);
-	
-	// Body Cluster 3
-	const std::string body_cluster_name_3 = "knee-ankle-differential";
-	auto joint_3 = std::make_shared<GeneralizedJoints::TelloKneeAnkleDifferential>(
-		knee_ankle_rotor_left, knee_ankle_rotor_right, shin, foot,
-		CoordinateAxis::Z, CoordinateAxis::Z,
-		CoordinateAxis::Y, CoordinateAxis::Y);
-	model.appendRegisteredBodiesAsCluster(body_cluster_name_3, joint_3);
+	    // Shin
+	    const SpatialTransform shin_Xtree = i == 0 ? SpatialTransform(R_left_shin,
+									  p_left_shin)
+						       : SpatialTransform(R_right_shin,
+									  p_right_shin);
+	    const std::string shin_name = side + "-shin";
+	    const std::string shin_parent_name = side + "-thigh";
+	    const SpatialInertia<double> shin_spatial_inertia = SpatialInertia<double>{shin_mass,
+		shin_CoM, shin_inertia};
+	    auto shin = model.registerBody(shin_name, shin_spatial_inertia, shin_parent_name, shin_Xtree);
+
+	    // Foot
+	    const SpatialTransform foot_Xtree = i == 0 ? SpatialTransform(R_left_foot,
+									  p_left_foot)
+						       : SpatialTransform(R_right_foot,
+									  p_right_foot);
+	    const std::string foot_name = side + "-foot";
+	    const std::string foot_parent_name = side + "-shin";
+	    const SpatialInertia<double> foot_spatial_inertia = SpatialInertia<double>{foot_mass,
+		foot_CoM, foot_inertia};
+	    auto foot = model.registerBody(foot_name, foot_spatial_inertia, foot_parent_name, foot_Xtree);
+
+	    // Hip differential cluster
+	    const std::string knee_ankle_differential_cluster_name = side + "-knee-ankle-differential";
+	    auto knee_ankle_differential_generalized_joint = 
+		std::make_shared<GeneralizedJoints::TelloKneeAnkleDifferential>(
+			knee_ankle_rotor_1, knee_ankle_rotor_2, shin, foot,
+			CoordinateAxis::Z, CoordinateAxis::Z, CoordinateAxis::Y, CoordinateAxis::Y);
+	    model.appendRegisteredBodiesAsCluster(knee_ankle_differential_cluster_name,
+						  knee_ankle_differential_generalized_joint);
+	}
 
 	return model;
     }
--- Robots/Tello.hpp
+++ Robots/Tello.hpp
@@ -15,43 +15,71 @@ namespace grbda
     private:
 	const double grav = 9.81;
 
-	const Mat3<double> R_hip_clamp = Mat3<double>::Identity();
-	const Vec3<double> p_hip_clamp = Vec3<double>{0., 126e-3, -87e-3};
+	// Left leg
+	const Mat3<double> R_left_hip_clamp = Mat3<double>::Identity();
+	const Vec3<double> p_left_hip_clamp = Vec3<double>{0., 126e-3, -87e-3};
 
-	const Mat3<double> R_hip_clamp_rotor = Mat3<double>::Identity();
-	const Vec3<double> p_hip_clamp_rotor = Vec3<double>{0., 126e-3, -26e-3};
+	const Mat3<double> R_left_gimbal = Mat3<double>::Identity();
+	const Vec3<double> p_left_gimbal = Vec3<double>{0., 0., -142.5e-3};
 
-	const Mat3<double> R_gimbal = Mat3<double>::Identity();
-	const Vec3<double> p_gimbal = Vec3<double>{0., 0., -142.5e-3};
+	const Mat3<double> R_left_thigh = Mat3<double>::Identity();
+	const Vec3<double> p_left_thigh = Vec3<double>::Zero();
 
-	const Mat3<double> R_thigh = Mat3<double>::Identity();
-	const Vec3<double> p_thigh = Vec3<double>{0., 0., 0.};
+	const Mat3<double> R_left_shin = Mat3<double>::Identity();
+	const Vec3<double> p_left_shin = Vec3<double>{0., 0., -226.8e-3};
 
-	const Mat3<double> R_hip_rotor_left = (Mat3<double>() << -1., 0., 0., 0., 0., 1., 0., 1., 0.).finished();
-	const Vec3<double> p_hip_rotor_left = Vec3<double>{0., 0.04, 0.};
+	const Mat3<double> R_left_foot = Mat3<double>::Identity();
+	const Vec3<double> p_left_foot = Vec3<double>{0., 0., -260e-3};
 
-	const Mat3<double> R_hip_rotor_right = R_hip_rotor_left;
-	const Vec3<double> p_hip_rotor_right = Vec3<double>{0., -0.04, 0.};
+	const Mat3<double> R_left_hip_clamp_rotor = Mat3<double>::Identity();
+	const Vec3<double> p_left_hip_clamp_rotor = Vec3<double>{0., 126e-3, -26e-3};
 
-	const Mat3<double> R_shin = Mat3<double>::Identity();
-	const Vec3<double> p_shin = Vec3<double>{0., 0., -226.8e-3};
+	const Mat3<double> R_left_hip_rotor_1 = (Mat3<double>() << -1., 0., 0., 0., 0., 1., 0., 1., 0.).finished();
+	const Vec3<double> p_left_hip_rotor_1 = Vec3<double>{0., 0.04, 0.};
 
-	const Mat3<double> R_foot = Mat3<double>::Identity();
-	const Vec3<double> p_foot = Vec3<double>{0., 0., -260e-3};
+	const Mat3<double> R_left_hip_rotor_2 = R_left_hip_rotor_1;
+	const Vec3<double> p_left_hip_rotor_2 = Vec3<double>{0., -0.04, 0.};
 
-	const Mat3<double> R_knee_ankle_rotor_left = R_hip_rotor_left;
-	const Vec3<double> p_knee_ankle_rotor_left = Vec3<double>{0., 26.55e-3, 0.};
+	const Mat3<double> R_left_knee_ankle_rotor_1 = R_left_hip_rotor_1;
+	const Vec3<double> p_left_knee_ankle_rotor_1 = Vec3<double>{0., 26.55e-3, 0.};
 
-	const Mat3<double> R_knee_ankle_rotor_right = R_hip_rotor_left;
-	const Vec3<double> p_knee_ankle_rotor_right = Vec3<double>{0., -26.55e-3, 0.};
+	const Mat3<double> R_left_knee_ankle_rotor_2 = R_left_hip_rotor_1;
+	const Vec3<double> p_left_knee_ankle_rotor_2 = Vec3<double>{0., -26.55e-3, 0.};
+
+	// Right leg
+	const Mat3<double> R_right_hip_clamp = Mat3<double>::Identity();
+	const Vec3<double> p_right_hip_clamp = Vec3<double>{0., -126e-3, -87e-3};
+
+	const Mat3<double> R_right_gimbal = Mat3<double>::Identity();
+	const Vec3<double> p_right_gimbal = Vec3<double>{0., 0., -142.5e-3};
+
+	const Mat3<double> R_right_thigh = Mat3<double>::Identity();
+	const Vec3<double> p_right_thigh = Vec3<double>::Zero();
+
+	const Mat3<double> R_right_shin = Mat3<double>::Identity();
+	const Vec3<double> p_right_shin = Vec3<double>{0., 0., -226.8e-3};
+
+	const Mat3<double> R_right_foot = Mat3<double>::Identity();
+	const Vec3<double> p_right_foot = Vec3<double>{0., 0., -260e-3};
+
+	const Mat3<double> R_right_hip_clamp_rotor = Mat3<double>::Identity();
+	const Vec3<double> p_right_hip_clamp_rotor = Vec3<double>{0., -126e-3, -26e-3};
+
+	const Mat3<double> R_right_hip_rotor_1 = (Mat3<double>() << -1., 0., 0., 0., 0., 1., 0., 1., 0.).finished();
+	const Vec3<double> p_right_hip_rotor_1 = Vec3<double>{0., 0.04, 0.};
+
+	const Mat3<double> R_right_hip_rotor_2 = R_right_hip_rotor_1;
+	const Vec3<double> p_right_hip_rotor_2 = Vec3<double>{0., -0.04, 0.};
+
+	const Mat3<double> R_right_knee_ankle_rotor_1 = R_right_hip_rotor_1;
+	const Vec3<double> p_right_knee_ankle_rotor_1 = Vec3<double>{0., 26.55e-3, 0.};
+
+	const Mat3<double> R_right_knee_ankle_rotor_2 = R_right_hip_rotor_1;
+	const Vec3<double> p_right_knee_ankle_rotor_2 = Vec3<double>{0., -26.55e-3, 0.};
 
 	const double hip_clamp_mass = 1.3289;
 	const Vec3<double> hip_clamp_CoM = Vec3<double>{-0.0010, 0., -0.0069};
 	const Mat3<double> hip_clamp_inertia = (Mat3<double>() << 0.0032, 0., 0.0001, 0., 0.0033, 0., 0.0001, 0., 0.0027).finished();
-
-	const double hip_clamp_rotor_mass = 1.;
-	const Vec3<double> hip_clamp_rotor_CoM = Vec3<double>{0., 0., 0.};
-	const Mat3<double> hip_clamp_rotor_inertia = (Mat3<double>() << 0., 0., 0., 0., 0., 0., 0., 0., 0.003).finished();
 
 	const double gimbal_mass = 0.4433;
 	const Vec3<double> gimbal_CoM = Vec3<double>{-0.0027, 0., 0.0258};
@@ -61,14 +89,6 @@ namespace grbda
 	const Vec3<double> thigh_CoM = Vec3<double>{0.003, -0.0001, -0.0323};
 	const Mat3<double> thigh_inertia = (Mat3<double>() << 0.0103, 0., -0.0005, 0., 0.0097, 0., -0.0005, 0., 0.0027).finished();
 
-	const double hip_rotor_left_mass = 1.;
-	const Vec3<double> hip_rotor_left_CoM = Vec3<double>{0., 0., 0.};
-	const Mat3<double> hip_rotor_left_inertia = (Mat3<double>() << 0., 0., 0., 0., 0., 0., 0., 0., 0.003).finished();
-
-	const double hip_rotor_right_mass = 1.;
-	const Vec3<double> hip_rotor_right_CoM = Vec3<double>{0., 0., 0.};
-	const Mat3<double> hip_rotor_right_inertia = (Mat3<double>() << 0., 0., 0., 0., 0., 0., 0., 0., 0.003).finished();
-
 	const double shin_mass = 0.3072;
 	const Vec3<double> shin_CoM = Vec3<double>{0.0047, -0.0003, -0.1043};
 	const Mat3<double> shin_inertia = (Mat3<double>() << 0.0054 , -0., -0.0002, -0., 0.0054, 0., -0.0002, 0., 0.0001).finished();
@@ -77,13 +97,25 @@ namespace grbda
 	const Vec3<double> foot_CoM = Vec3<double>{0.0042, -0., -0.0251};
 	const Mat3<double> foot_inertia = (Mat3<double>() << 0.094e-3, -0., -0.0038e-3, -0., 0.1773e-3, 0., -0.0038e-3, 0., 0.0901e-3).finished();
 
-	const double knee_ankle_rotor_left_mass = 1.;
-	const Vec3<double> knee_ankle_rotor_left_CoM = Vec3<double>{0., 0., 0.};
-	const Mat3<double> knee_ankle_rotor_left_inertia = (Mat3<double>() << 0., 0., 0., 0., 0., 0., 0., 0., 0.003).finished();
+	const double hip_clamp_rotor_mass = 1.;
+	const Vec3<double> hip_clamp_rotor_CoM = Vec3<double>{0., 0., 0.};
+	const Mat3<double> hip_clamp_rotor_inertia = (Mat3<double>() << 0., 0., 0., 0., 0., 0., 0., 0., 0.003).finished();
 
-	const double knee_ankle_rotor_right_mass = 1.;
-	const Vec3<double> knee_ankle_rotor_right_CoM = Vec3<double>{0., 0., 0.};
-	const Mat3<double> knee_ankle_rotor_right_inertia = (Mat3<double>() << 0., 0., 0., 0., 0., 0., 0., 0., 0.003).finished();
+	const double hip_rotor_1_mass = 1.;
+	const Vec3<double> hip_rotor_1_CoM = Vec3<double>{0., 0., 0.};
+	const Mat3<double> hip_rotor_1_inertia = (Mat3<double>() << 0., 0., 0., 0., 0., 0., 0., 0., 0.003).finished();
+
+	const double hip_rotor_2_mass = 1.;
+	const Vec3<double> hip_rotor_2_CoM = Vec3<double>{0., 0., 0.};
+	const Mat3<double> hip_rotor_2_inertia = (Mat3<double>() << 0., 0., 0., 0., 0., 0., 0., 0., 0.003).finished();
+
+	const double knee_ankle_rotor_1_mass = 1.;
+	const Vec3<double> knee_ankle_rotor_1_CoM = Vec3<double>{0., 0., 0.};
+	const Mat3<double> knee_ankle_rotor_1_inertia = (Mat3<double>() << 0., 0., 0., 0., 0., 0., 0., 0., 0.003).finished();
+
+	const double knee_ankle_rotor_2_mass = 1.;
+	const Vec3<double> knee_ankle_rotor_2_CoM = Vec3<double>{0., 0., 0.};
+	const Mat3<double> knee_ankle_rotor_2_inertia = (Mat3<double>() << 0., 0., 0., 0., 0., 0., 0., 0., 0.003).finished();
     };
 
 } // namespace grbda
