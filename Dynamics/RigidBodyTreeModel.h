d7567ca641a2f0852e1b7a33e80d7be917b43bdd
matt <mchigs41@gmail.com>
2023-07-06 17:38:19 -0400

    Set state for rigid body model tree

--- Dynamics/RigidBodyTreeModel.h
+++ Dynamics/RigidBodyTreeModel.h
@@ -73,6 +73,26 @@ namespace grbda
             forward_dynamics_method_ = fd_method;
         }
 
+        // TODO(@MatthewChignoli): These are functions and members shared with FloatingBaseModel. Not sure how I want to deal with them moving forward. It's unclear which parts of Robot-Software need to change for compatiblity with GRBDA and which parts of GRBDA need to change for compatibility with Robot-Software. Should these functions be abstraced to TreeModel since ClusterTreeModel also uses them?
+
+        // TODO(@MatthewChignoli): We currently assume that the state is given as independent coordinates.
+        void setState(const DVec<double> &state)
+        {
+            const int &nq = ceil(state.size() / 2.0);
+            const int &nv = floor(state.size() / 2.0);
+
+            const DVec<double> q = gamma_(state.head(nq));
+            const DVec<double> qd = G_ * state.tail(nv);
+
+            std::cout << "State: " << state.transpose() << std::endl;
+            std::cout << "q, qd: " << q.transpose() << ", " << qd.transpose() << std::endl;
+
+            initializeState(q, qd);
+        }
+
+        /////////////////////////////////////
+
+
         int getNumBodies() const override { return (int)rigid_body_nodes_.size(); }
 
         const Body &getBody(int index) const override { return rigid_body_nodes_[index]->body_; }
