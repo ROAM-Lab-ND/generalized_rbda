aaa241f8f7efcf578c7b4017f860ca073950843a
nicholasadr <adr.nicholas@gmail.com>
2023-07-06 11:32:45 -0400

    Abstract Tello differential generalized joint

--- Dynamics/GeneralizedJoints/TelloHipDifferential.h
+++ Dynamics/GeneralizedJoints/TelloHipDifferential.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "GeneralizedJoint.h"
+#include "TelloDifferential.h"
 #include "3rd-parties/CasadiGen/header/CasadiGen.h"
 
 namespace grbda
@@ -9,38 +9,37 @@ namespace grbda
     namespace GeneralizedJoints
     {
 
-	class TelloHipDifferential : public Base
+	class TelloHipDifferential : public TelloDifferential
 	{
 	public:
 	    TelloHipDifferential(Body &rotor_1, Body &rotor_2, Body &link_1, Body &link_2,
 				 CoordinateAxis rotor_axis_1, CoordinateAxis rotor_axis_2,
-				 CoordinateAxis joint_axis_1, CoordinateAxis joint_axis_2);
+				 CoordinateAxis joint_axis_1, CoordinateAxis joint_axis_2)
+				 : TelloDifferential(rotor_1, rotor_2, link_1, link_2,
+				 rotor_axis_1, rotor_axis_2, joint_axis_1, joint_axis_2)
+	    {
+		td_kikd = thd_kikd;
+		td_kikd_sparsity_out = thd_kikd_sparsity_out;
+		td_kikd_work = thd_kikd_work;
+		td_J_dy_2_dqd = thd_J_dy_2_dqd;
+		td_J_dy_2_dqd_sparsity_out = thd_J_dy_2_dqd_sparsity_out;
+		td_J_dy_2_dqd_work = thd_J_dy_2_dqd_work;
+		td_g = thd_g;
+		td_g_sparsity_out = thd_g_sparsity_out;
+		td_g_work = thd_g_work;
+		td_k = thd_k;
+		td_k_sparsity_out = thd_k_sparsity_out;
+		td_k_work = thd_k_work;
+		td_IK_pos = thd_IK_pos;
+		td_IK_pos_sparsity_out = thd_IK_pos_sparsity_out;
+		td_IK_pos_work = thd_IK_pos_work;
+		td_IK_vel = thd_IK_vel;
+		td_IK_vel_sparsity_out = thd_IK_vel_sparsity_out;
+		td_IK_vel_work = thd_IK_vel_work;
+	    }
 	    virtual ~TelloHipDifferential() {}
 
 	    GeneralizedJointTypes type() const override { return GeneralizedJointTypes::TelloHipDifferential; }
-
-	    void updateKinematics(const JointState &joint_state) override;
-
-	    void computeSpatialTransformFromParentToCurrentCluster(
-		GeneralizedSpatialTransform &Xup) const override;
-
-	    JointState randomJointState() const override;
-
-	private:
-	    void updateConstraintJacobians(const JointCoordinate &joint_pos) override;
-	    void updateConstraintBias(const JointState &joint_state) override;
-
-	    JointPtr rotor_1_joint_;
-	    JointPtr rotor_2_joint_;
-	    JointPtr link_1_joint_;
-	    JointPtr link_2_joint_;
-
-	    SpatialTransform X21_;
-	    
-	    const Body rotor_1_;
-	    const Body rotor_2_;
-	    const Body link_1_;
-	    const Body link_2_;
 
 	};
 
--- Dynamics/GeneralizedJoints/TelloKneeAnkleDifferential.h
+++ Dynamics/GeneralizedJoints/TelloKneeAnkleDifferential.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "GeneralizedJoint.h"
+#include "TelloDifferential.h"
 #include "3rd-parties/CasadiGen/header/CasadiGen.h"
 
 namespace grbda
@@ -9,39 +9,37 @@ namespace grbda
     namespace GeneralizedJoints
     {
 
-	class TelloKneeAnkleDifferential : public Base
+	class TelloKneeAnkleDifferential : public TelloDifferential
 	{
 	public:
 	    TelloKneeAnkleDifferential(Body &rotor_1, Body &rotor_2, Body &link_1, Body &link_2,
 				       CoordinateAxis rotor_axis_1, CoordinateAxis rotor_axis_2,
-				       CoordinateAxis joint_axis_1, CoordinateAxis joint_axis_2);
+				       CoordinateAxis joint_axis_1, CoordinateAxis joint_axis_2)
+				       : TelloDifferential(rotor_1, rotor_2, link_1, link_2,
+				       rotor_axis_1, rotor_axis_2, joint_axis_1, joint_axis_2)
+	    {
+		td_kikd = tkad_kikd;
+		td_kikd_sparsity_out = tkad_kikd_sparsity_out;
+		td_kikd_work = tkad_kikd_work;
+		td_J_dy_2_dqd = tkad_J_dy_2_dqd;
+		td_J_dy_2_dqd_sparsity_out = tkad_J_dy_2_dqd_sparsity_out;
+		td_J_dy_2_dqd_work = tkad_J_dy_2_dqd_work;
+		td_g = tkad_g;
+		td_g_sparsity_out = tkad_g_sparsity_out;
+		td_g_work = tkad_g_work;
+		td_k = tkad_k;
+		td_k_sparsity_out = tkad_k_sparsity_out;
+		td_k_work = tkad_k_work;
+		td_IK_pos = tkad_IK_pos;
+		td_IK_pos_sparsity_out = tkad_IK_pos_sparsity_out;
+		td_IK_pos_work = tkad_IK_pos_work;
+		td_IK_vel = tkad_IK_vel;
+		td_IK_vel_sparsity_out = tkad_IK_vel_sparsity_out;
+		td_IK_vel_work = tkad_IK_vel_work;
+	    }
 	    virtual ~TelloKneeAnkleDifferential() {}
 
 	    GeneralizedJointTypes type() const override { return GeneralizedJointTypes::TelloKneeAnkleDifferential; }
-
-	    void updateKinematics(const JointState &joint_state) override;
-
-	    void computeSpatialTransformFromParentToCurrentCluster(
-		GeneralizedSpatialTransform &Xup) const override;
-
-	    JointState randomJointState() const override;
-
-	private:
-	    void updateConstraintJacobians(const JointCoordinate &joint_pos) override;
-	    void updateConstraintBias(const JointState &joint_state) override;
-
-	    JointPtr rotor_1_joint_;
-	    JointPtr rotor_2_joint_;
-	    JointPtr link_1_joint_;
-	    JointPtr link_2_joint_;
-
-	    SpatialTransform X21_;
-	    
-	    const Body rotor_1_;
-	    const Body rotor_2_;
-	    const Body link_1_;
-	    const Body link_2_;
-
 	};
 
     }
