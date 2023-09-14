d6f17d1dba8c4c4357fa8ac51ba997a6f8dc942d
matt <mchigs41@gmail.com>
2023-09-14 10:22:14 -0400

    - Move matlab scripts to plotting folder - Improve run accuracy benchmark shell script - Delete all data and figures

--- Benchmarking/run_accuracy_benchmark.sh
+++ Benchmarking/run_accuracy_benchmark.sh
@@ -1,9 +1,41 @@
 #!/bin/bash
 
-# TODO(@MatthewChignoli): Two arguments, the first tells which accuracy benchmark to run, the second tells whether not to run the C++ binary
-
 # Flag to determine whether to run the C++ binary (default is 1)
-run_cpp=1
+benchmark=""
+
+# Check if at least one argument is provided
+if [[ $# -ge 1 ]]; then
+  # Parse the first argument as the benchmark
+  benchmark=$1
+else
+  echo "Usage: $0 [benchmark]"
+  echo -e "  benchmark: Specify which accuracy benchmark to run ('f', 'i', 'a', or 'r')\n"
+  exit 1
+fi
+
+# Check if the benchmark argument is valid
+case "$benchmark" in
+  "f" | "F" | "fd" | "FD")
+    benchmark="fd"
+    ;;
+  "i" | "I" | "id" | "ID")
+    benchmark="id"
+    ;;
+  "a" | "A" | "atf" | "ATF")
+    benchmark="atf"
+    ;;
+  "r" | "R" | "robots" | "Robots")
+    benchmark="robots"
+    ;;
+  *)
+    echo "Invalid benchmark argument. Choose one of 'f', 'i', 'a', or 'r'."
+    exit 1
+    ;;
+esac
+
+# Run the binary
+./bin/accuracy_benchmark_$benchmark
+
 
 # Check if an argument is provided to the script
 if [[ $# -gt 0 ]]; then
@@ -14,13 +46,5 @@ else
   echo -e "  run_cpp: 0 to skip running the C++ binary, 1 (default) to run it\n\n"
 fi
 
-# Run the C++ binary if the flag is set to 1
-if [[ $run_cpp -eq 1 ]]; then
-    # ./bin/accuracy_benchmark_fd
-    # ./bin/accuracy_benchmark_id
-    # ./bin/accuracy_benchmark_atf
-    ./bin/accuracy_benchmark_robots
-fi
-
 # Run MATLAB and execute the script to plot the results
-matlab -nodisplay -nosplash -nodesktop -r "try, addpath('../Benchmarking'); accuracy_benchmark_robots; catch ME, disp(ME.message), exit(1), end; exit(0);"
+matlab -nodisplay -nosplash -nodesktop -r "try, addpath('../Benchmarking/plotting'); accuracy_benchmark_$benchmark; catch ME, disp(ME.message), exit(1), end; exit(0);"
