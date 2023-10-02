e86be7e33aa3f3a309c78d82b2c4498e79bdd2c6
matt <mchigs41@gmail.com>
2023-10-02 14:56:52 -0400

    RNEA autodiff unit test passes

--- UnitTests/testAutoDiff.cpp
+++ UnitTests/testAutoDiff.cpp
@@ -184,6 +184,7 @@ protected:
             ClusterTreeModel<SX> model = robot.buildClusterTreeModel();
 
             createContactJacobianCasadiFunctions(model);
+            createRneaCasadiFunctions(model);
 
             if (i == 0)
             {
@@ -262,9 +263,39 @@ protected:
         contact_jacobian_fcn_maps_.push_back(contact_jacobian_fcns);
     }
 
+    void createRneaCasadiFunctions(ClusterTreeModel<casadi::SX> model)
+    {
+        typedef casadi::Sparsity Sparsity;
+
+        SX cs_q_sym = SX::sym("q", model.getNumPositions(), 1);
+        SX cs_dq_sym = SX::sym("dq", model.getNumDegreesOfFreedom(), 1);
+        SX cs_qd_sym = SX::sym("qd", model.getNumDegreesOfFreedom(), 1);
+        ModelState<SX> state = createSymbolicModelState(model, cs_q_sym, cs_dq_sym, cs_qd_sym);
+
+        model.setState(state);
+
+        SX cs_tau_sym = SX::sym("tau", model.getNumDegreesOfFreedom(), 1);
+        DVec<SX> tau_sym(model.getNumDegreesOfFreedom());
+        casadi::copy(cs_tau_sym, tau_sym);
+
+        DVec<SX> qdd_sym = model.inverseDynamics(tau_sym);
+        SX cs_qdd_sym = SX(Sparsity::dense(qdd_sym.size(), 1));
+        casadi::copy(qdd_sym, cs_qdd_sym);
+
+        SX dqdd_ddq = jacobian(cs_qdd_sym, cs_dq_sym);
+        SX dqdd_dqd = jacobian(cs_qdd_sym, cs_qd_sym);
+        SX dqdd_dtau = jacobian(cs_qdd_sym, cs_tau_sym);
+
+        std::vector<SX> args{cs_q_sym, cs_dq_sym, cs_qd_sym, cs_tau_sym};
+        std::vector<SX> res{cs_qdd_sym, dqdd_ddq, dqdd_dqd, dqdd_dtau};
+        casadi::Function rneaFunction("rnea", args, res);
+        rnea_fcns_.push_back(rneaFunction);
+    }
+
     int nq_, nv_;
     const int num_robots_ = 4;
     std::vector<std::unordered_map<std::string, casadi::Function>> contact_jacobian_fcn_maps_;
+    std::vector<casadi::Function> rnea_fcns_;
 };
 
 using testing::Types;
@@ -326,7 +357,7 @@ TYPED_TEST(AutoDiffRobotTest, contactJacobians)
                 for (int j = 0; j < this->nv_; j++)
                 {
                     std::vector<DM> dq_plus = dq;
-                    dq_plus[j] += 1e-8;
+                    dq_plus[j] += h;
                     std::vector<DM> q_plus = TestHelpers::plus(q, dq_plus);
 
                     std::vector<DM> res_plus = fcn(std::vector<DM>{q_plus, dq});
@@ -334,7 +365,7 @@ TYPED_TEST(AutoDiffRobotTest, contactJacobians)
                     casadi::copy(res_plus[2], contact_point_pos_plus);
 
                     std::vector<DM> dq_minus = dq;
-                    dq_minus[j] -= 1e-8;
+                    dq_minus[j] -= h;
                     std::vector<DM> q_minus = TestHelpers::plus(q, dq_minus);
 
                     std::vector<DM> res_minus = fcn(std::vector<DM>{q_minus, dq});
@@ -347,6 +378,120 @@ TYPED_TEST(AutoDiffRobotTest, contactJacobians)
 
                 GTEST_ASSERT_LE((contact_point_jac_fd - contact_point_jac_full).norm(), 1e-6);
             }
+        }
+    }
+}
+
+TYPED_TEST(AutoDiffRobotTest, rnea)
+{
+    // This test validates that the partial derivatives of the inverse dynamics with respect to q,
+    // qd, and tau and the same when computed via autodiff and finite difference
+
+    using DM = casadi::DM;
+
+    for (const casadi::Function &fcn : this->rnea_fcns_)
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            // Random state
+            std::vector<DM> q = math::random<DM>(this->nq_);
+            std::vector<DM> dq = math::zeros<DM>(this->nv_);
+            std::vector<DM> qd = math::random<DM>(this->nv_);
+            std::vector<DM> tau = math::random<DM>(this->nv_);
+
+            // TODO(@MatthewChignoli): Fix this current method for dealing with floating base robots
+            if (this->nq_ != this->nv_)
+            {
+                Quat<double> quat = ori::rpyToQuat(Vec3<double>::Random());
+                q[3] = quat[0];
+                q[4] = quat[1];
+                q[5] = quat[2];
+                q[6] = quat[3];
+            }
+
+            // Autodiff
+            DMat<double> dqdd_ddq_ad(this->nv_, this->nv_);
+            DMat<double> dqdd_dqd_ad(this->nv_, this->nv_);
+            DMat<double> dqdd_dtau_ad(this->nv_, this->nv_);
+
+            std::vector<DM> res = fcn(std::vector<DM>{q, dq, qd, tau});
+
+            casadi::copy(res[1], dqdd_ddq_ad);
+            casadi::copy(res[2], dqdd_dqd_ad);
+            casadi::copy(res[3], dqdd_dtau_ad);
+
+            // Finite Difference
+            double h = 1e-8;
+
+            // Partial with respect to dq
+            DMat<double> dqdd_ddq_fd(this->nv_, this->nv_);
+            for (int j = 0; j < this->nv_; j++)
+            {
+                std::vector<DM> dq_plus = dq;
+                dq_plus[j] += h;
+                std::vector<DM> q_plus = TestHelpers::plus(q, dq_plus);
+
+                std::vector<DM> res_plus = fcn(std::vector<DM>{q_plus, dq, qd, tau});
+                DVec<double> qdd_plus(this->nv_);
+                casadi::copy(res_plus[0], qdd_plus);
+
+                std::vector<DM> dq_minus = dq;
+                dq_minus[j] -= h;
+                std::vector<DM> q_minus = TestHelpers::plus(q, dq_minus);
+
+                std::vector<DM> res_minus = fcn(std::vector<DM>{q_minus, dq, qd, tau});
+                DVec<double> qdd_minus(this->nv_);
+                casadi::copy(res_minus[0], qdd_minus);
+
+                dqdd_ddq_fd.col(j) = (qdd_plus - qdd_minus) / (2 * h);
+            }
+            GTEST_ASSERT_LE((dqdd_ddq_ad - dqdd_ddq_fd).norm(), 1e-5);
+
+            // Partial with respect to dq
+            DMat<double> dqdd_dqd_fd(this->nv_, this->nv_);
+            for (int j = 0; j < this->nv_; j++)
+            {
+                // Partial with respect to qd
+                std::vector<DM> qd_plus = qd;
+                qd_plus[j] += h;
+
+                std::vector<DM> res_plus = fcn(std::vector<DM>{q, dq, qd_plus, tau});
+                DVec<double> qdd_plus(this->nv_);
+                casadi::copy(res_plus[0], qdd_plus);
+
+                std::vector<DM> qd_minus = qd;
+                qd_minus[j] -= h;
+
+                std::vector<DM> res_minus = fcn(std::vector<DM>{q, dq, qd_minus, tau});
+                DVec<double> qdd_minus(this->nv_);
+                casadi::copy(res_minus[0], qdd_minus);
+
+                dqdd_dqd_fd.col(j) = (qdd_plus - qdd_minus) / (2 * h);
+            }
+            GTEST_ASSERT_LE((dqdd_dqd_ad - dqdd_dqd_fd).norm(), 1e-5);
+
+            // Partial with respect to tau
+            DMat<double> dqdd_dtau_fd(this->nv_, this->nv_);
+            for (int j = 0; j < this->nv_; j++)
+            {
+                // Partial with respect to tau
+                std::vector<DM> tau_plus = tau;
+                tau_plus[j] += h;
+
+                std::vector<DM> res_plus = fcn(std::vector<DM>{q, dq, qd, tau_plus});
+                DVec<double> qdd_plus(this->nv_);
+                casadi::copy(res_plus[0], qdd_plus);
+
+                std::vector<DM> tau_minus = tau;
+                tau_minus[j] -= h;
+
+                std::vector<DM> res_minus = fcn(std::vector<DM>{q, dq, qd, tau_minus});
+                DVec<double> qdd_minus(this->nv_);
+                casadi::copy(res_minus[0], qdd_minus);
+
+                dqdd_dtau_fd.col(j) = (qdd_plus - qdd_minus) / (2 * h);
+            }
+            GTEST_ASSERT_LE((dqdd_dtau_ad - dqdd_dtau_fd).norm(), 1e-5);
         }
     }
 }
