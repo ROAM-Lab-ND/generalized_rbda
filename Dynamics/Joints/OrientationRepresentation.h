d9261e8be7057dde542cf43d0a98de7cf4229964
Charles Khazoom <charles.khazoom@hotmail.com>
2023-10-06 11:26:52 -0400

    template OrientationReprensentation on scalar

--- Dynamics/Joints/OrientationRepresentation.h
+++ Dynamics/Joints/OrientationRepresentation.h
@@ -8,32 +8,35 @@ namespace grbda
 {
     namespace ori_representation
     {
+        template <typename T = double>
         struct Base {
             static const int num_ori_parameter;      
             static const int numSpanningPos;         
             static const int numIndependentPos;
 
-            virtual RotMat<double> getRotationMatrix(const DVec<double>& q) const = 0; // pure virtual method
+            virtual RotMat<T> getRotationMatrix(const DVec<T>& q) const = 0; // pure virtual method
         };
 
-        struct Quaternion : public Base {
+        template <typename T = double>
+        struct Quaternion : public Base<T> {`
             static const int num_ori_parameter = 4;
             static const int numSpanningPos = 7;
             static const int numIndependentPos = 7;
 
-            RotMat<double> getRotationMatrix(const DVec<double>& q) const override {
+            RotMat<T> getRotationMatrix(const DVec<T>& q) const override {
                 const RotMat<Scalar> R = ori::quaternionToRotationMatrix(q.template tail<4>());
                 // ... 
                 return rotationMatrix;
             }
         };
 
-        struct RollPitchYaw : public Base {
+        template <typename T = double>
+        struct RollPitchYaw : public Base<T> {
             static const int num_ori_parameter = 3;
             static const int numSpanningPos = 6;
             static const int numIndependentPos = 6;
 
-            RotMat<double> getRotationMatrix(const DVec<double>& q) const override {
+            RotMat<T> getRotationMatrix(const DVec<T>& q) const override {
                 return ori::rpyToRotMat(q);
             }
         };
