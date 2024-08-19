c3b6fb7527231dea02806c16f06623bd0aa03986
MatthewChignoli <matthew.chignoli@gmail.com>
2024-08-19 12:32:45 -0400

    Debug explicit benchmark joint ordering

--- Benchmarking/src/forwardDynamicsBenchmarkExplicit.cpp
+++ Benchmarking/src/forwardDynamicsBenchmarkExplicit.cpp
@@ -27,8 +27,7 @@ int main()
     const int nv = cluster_tree.getNumDegreesOfFreedom();
 
     grbda::ModelState<double> model_state;
-    Eigen::VectorXd spanning_joint_pos = Eigen::VectorXd::Zero(0);
-    Eigen::VectorXd spanning_joint_vel = Eigen::VectorXd::Zero(0);
+    grbda::ModelState<double> spanning_model_state;
 
     Eigen::MatrixXd K_cluster = Eigen::MatrixXd::Zero(0,0);
     Eigen::VectorXd k_cluster = Eigen::VectorXd::Zero(0);
@@ -40,14 +39,8 @@ int main()
         grbda::JointState<double> joint_state = cluster->joint_->randomJointState();
         grbda::JointState<double> spanning_joint_state = cluster->joint_->toSpanningTreeState(joint_state);
 
-        spanning_joint_pos = appendEigenVector(spanning_joint_pos,
-                                               spanning_joint_state.position);
-        spanning_joint_vel = appendEigenVector(spanning_joint_vel,
-                                               spanning_joint_state.velocity);
-
         model_state.push_back(joint_state);
-        std::cout << "cluster_pos: " << joint_state.position << std::endl;
-        std::cout << "cluster_vel: " << joint_state.velocity << std::endl;
+        spanning_model_state.push_back(spanning_joint_state);
 
         K_cluster = grbda::appendEigenMatrix(K_cluster, cluster->joint_->K());
         k_cluster = grbda::appendEigenVector(k_cluster, cluster->joint_->k());
@@ -64,10 +57,6 @@ int main()
 
     std::cout << "cluster_tau: \n" << tau << std::endl;
 
-    std::cout << "spanning_joint_pos: \n" << spanning_joint_pos << std::endl;
-    std::cout << "spanning_joint_vel: \n" << spanning_joint_vel << std::endl;
-    std::cout << "spanning_joint_tau: \n" << spanning_joint_tau << std::endl;
-
     //std::cout << "cluster tau verification: " << tau_2 << std::endl;
 
     std::cout << "K cluster: \n" << K_cluster << std::endl;
@@ -76,13 +65,45 @@ int main()
     std::cout << "G_transpose_pinv_cluster: \n" << G_transpose_pinv << std::endl;
 
     cluster_tree.setState(model_state);
+    auto spanning_q_and_v = grbda::modelStateToVector(spanning_model_state);
+    Eigen::VectorXd spanning_joint_pos = spanning_q_and_v.first;
+    Eigen::VectorXd spanning_joint_vel = spanning_q_and_v.second;
+
+    // Start with the forward kinematics
+    Eigen::VectorXd pin_q(6), pin_v(6), pin_tau(6);
+    pin_q << spanning_joint_pos[0], spanning_joint_pos[2], spanning_joint_pos[4],
+        spanning_joint_pos[5], spanning_joint_pos[3], spanning_joint_pos[1];
+    pin_v << spanning_joint_vel[0], spanning_joint_vel[2], spanning_joint_vel[4],
+        spanning_joint_vel[5], spanning_joint_vel[3], spanning_joint_vel[1];
+    pin_tau << spanning_joint_tau[0], spanning_joint_tau[2], spanning_joint_tau[4],
+        spanning_joint_tau[5], spanning_joint_tau[3], spanning_joint_tau[1];
+
+    pinocchio::forwardKinematics(model, data, pin_q, pin_v);
+    cluster_tree.forwardKinematics();
+
+    std::cout << "Pinocchio Model Joints Names" << std::endl;
+    for (int i = 0; i < model.njoints; i++)
+    {
+        std::cout << model.names[i] << std::endl;
+    }
+
+    // Joint 1 Velocity
+    std::cout << "Joint 1 Velocity (model)       : " << data.v[1].angular().transpose() << " " << data.v[1].linear().transpose() << std::endl;
+    std::cout << "Joint 1 Velocity (cluster_tree): " << cluster_tree.cluster(0)->v_.head<6>().transpose() << std::endl;
+
+    // Joint 2 Velocity
+    std::cout << "Joint 2 Velocity (model)       : " << data.v[2].angular().transpose() << " " << data.v[2].linear().transpose() << std::endl;
+    std::cout << "Joint 2 Velocity (cluster_tree): " << cluster_tree.cluster(1)->v_.head<6>().transpose() << std::endl;
+
+    // Joint 3 Velocity
+    std::cout << "Joint 3 Velocity (model)       : " << data.v[3].angular().transpose() << " " << data.v[3].linear().transpose() << std::endl;
+    std::cout << "Joint 3 Velocity (cluster_tree): " << cluster_tree.cluster(2)->v_.head<6>().transpose() << std::endl;
 
     const Eigen::VectorXd qdd_cluster = cluster_tree.forwardDynamics(tau);
     std::cout << "qdd_cluster: \n" << qdd_cluster << std::endl;
 
     const double mu0 = 1e-12;
-    pinocchio::forwardDynamics(model, data, spanning_joint_pos, spanning_joint_vel,
-        spanning_joint_tau, K_cluster, k_cluster, mu0);
+    pinocchio::forwardDynamics(model, data, pin_q, pin_v, pin_tau, K_cluster, k_cluster, mu0);
     std::cout << "qdd_pinocchio: \n" << data.ddq << std::endl;
     
     lg_mult_eigen_model.setState(spanning_joint_pos, spanning_joint_vel);
