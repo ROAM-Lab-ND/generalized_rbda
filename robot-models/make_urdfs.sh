8041761c20bdd76b41b559c3b038d3e1eb82c969
nicholasadr <adr.nicholas@gmail.com>
2024-08-31 10:48:42 -0400

    Add variable-sized revolute rotor in chain and parallel

--- robot-models/make_urdfs.sh
+++ robot-models/make_urdfs.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
+link_count=${1:-3}
+
 # get the current directory
 CURRENTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # xacros all robots
 cd $CURRENTDIR
+
+VAR_REVOLUTE_DIR="variable_revolute_urdf"
+mkdir "$VAR_REVOLUTE_DIR"
+rm "$VAR_REVOLUTE_DIR"/*
+for (( i=link_count; i>0; i-- ))
+do
+    xacro revolute_rotor_parallel.xacro loop_count:=$i > "$VAR_REVOLUTE_DIR/revolute_rotor_parallel_${i}.urdf"
+    xacro revolute_rotor_chain.xacro loop_count:=$i > "$VAR_REVOLUTE_DIR/revolute_rotor_chain_${i}.urdf"
+done
+
 xacro mini_cheetah.xacro > mini_cheetah.urdf
 xacro mini_cheetah_fr_leg.xacro > mini_cheetah_fr_leg.urdf
 xacro mini_cheetah_fl_leg.xacro > mini_cheetah_fl_leg.urdf
