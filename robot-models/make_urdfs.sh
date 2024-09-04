004b77ee6f3a1dadf6a1dabd3b629e9805d0ebc9
MatthewChignoli <matthew.chignoli@gmail.com>
2024-09-04 09:41:40 -0400

    WIP parallel chains benchmark

--- robot-models/make_urdfs.sh
+++ robot-models/make_urdfs.sh
@@ -29,3 +29,4 @@ xacro mini_cheetah_hr_leg.xacro > mini_cheetah_hr_leg.urdf
 xacro mini_cheetah_hl_leg.xacro > mini_cheetah_hl_leg.urdf
 xacro mit_humanoid_leg.xacro > mit_humanoid_leg.urdf
 xacro mit_humanoid.xacro > mit_humanoid.urdf
+xacro parallel_chains.xacro > parallel_chains.urdf
