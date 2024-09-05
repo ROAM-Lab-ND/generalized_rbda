e500423182ed791d56c0e0137920e68a0cf80190
nicholasadr <adr.nicholas@gmail.com>
2024-09-04 22:01:52 -0400

    Record FD instruction count for multiple systems:
    - make_urdfs.sh can generate multiple revolute_rotor_branch

--- robot-models/make_urdfs.sh
+++ robot-models/make_urdfs.sh
@@ -1,8 +1,37 @@
 #!/bin/bash
 
-link_count=${1:-5}
-branch_count=${2:-4}
-depth_count=${3:-2}
+# Initialize variables
+arg1="4" # revolute rotor serial/parallel loop count
+arg2=("1" "2" "4") # revolute rotor branch: branch count
+arg3=("1" "2" "3") # revolute rotor branch: depth count
+
+# Loop through arguments
+for arg in "$@"
+do
+    case $arg in
+        -sp=*|--serial-parallel=*)
+        arg1="${arg#*=}"
+        shift
+        ;;
+        -b=*|--branch_list=*)
+        IFS=',' read -r -a arg2 <<< "${arg#*=}"
+        shift
+        ;;
+        -d=*|--depth_list=*)
+        IFS=',' read -r -a arg3 <<< "${arg#*=}"
+        shift
+        ;;
+        *)
+        echo "Invalid argument: $arg" 1>&2
+        exit 1
+        ;;
+    esac
+done
+
+# Output the results
+echo "[revolute_rotor_chain/parallel] loop count: $arg1"
+echo "[revolute_rotor_branch] branch count: ${arg2[@]}"
+echo "[revolute_rotor_branch] depth count: ${arg3[@]}"
 
 # get the current directory
 CURRENTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
@@ -13,14 +42,17 @@ cd $CURRENTDIR
 VAR_REVOLUTE_DIR="variable_revolute_urdf"
 mkdir "$VAR_REVOLUTE_DIR"
 rm "$VAR_REVOLUTE_DIR"/*
-for (( i=link_count; i>0; i-- ))
+for (( i=arg1; i>0; i-- ))
 do
     xacro revolute_rotor_parallel.xacro loop_count:=$i > "$VAR_REVOLUTE_DIR/revolute_rotor_parallel_${i}.urdf"
     xacro revolute_rotor_chain.xacro loop_count:=$i > "$VAR_REVOLUTE_DIR/revolute_rotor_chain_${i}.urdf"
 done
-
-xacro revolute_rotor_branch.xacro branch_count:=${branch_count} depth_count:=${depth_count} > \
-      "$VAR_REVOLUTE_DIR/revolute_rotor_branch_${branch_count}_${depth_count}.urdf"
+for branch_count in "${arg2[@]}"; do
+    for depth_count in "${arg3[@]}"; do
+        xacro revolute_rotor_branch.xacro branch_count:=${branch_count} depth_count:=${depth_count} > \
+            "$VAR_REVOLUTE_DIR/revolute_rotor_branch_${branch_count}_${depth_count}.urdf"
+    done
+done
 
 xacro mini_cheetah.xacro > mini_cheetah.urdf
 xacro mini_cheetah_fr_leg.xacro > mini_cheetah_fr_leg.urdf
