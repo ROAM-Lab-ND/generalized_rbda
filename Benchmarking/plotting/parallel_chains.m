f4e6fd376933af5e512a166cdd01447654a78ec4
MatthewChignoli <matthew.chignoli@gmail.com>
2025-05-01 15:53:13 -0400

    Improved plotting

--- Benchmarking/plotting/parallel_chains.m
+++ Benchmarking/plotting/parallel_chains.m
@@ -2,8 +2,9 @@ close all; clear; clc;
 run plottingOptions.m
 
 %% Load Data
-% path_to_data = '../data/InstructionPinocchioFD_';
-path_to_data = '../data/TimingPinocchioFD_';
+data_type = 'Timing';
+% data_type = 'Instruction';
+path_to_data = ['../data/', data_type, 'PinocchioFD_'];
 
 expl_data = readmatrix([path_to_data, 'Explicit.csv']);
 impl_data = readmatrix([path_to_data, 'Implicit.csv']);
@@ -39,10 +40,10 @@ if length(impl_depths) ~= 4
 end
 
 %% Plot
-axes = [[1 6 8e2 2e4];
-         [1 9 2e3 1e5];
-         [1 16 3e3 1e6];
-         [1 21 1e4 1e6]];
+axes = [[1 6 2e-3 1e-1];
+         [1 9 4e-3 2e-1];
+         [1 16 1e-2 1e0];
+         [1 21 1e-2 2e0]];
 
 caba_data{1} = caba.expl;
 caba_data{2} = caba.impl;
@@ -68,25 +69,31 @@ pin_aba_data{2} = pin_aba.impl;
 data_names = {'Transmission', 'Connecting Rod'};
 
 figure
-size_scale = 3500;
-size_ratio = 0.5;
+size_scale = 2200;
+size_ratio = 0.6;
 set(gcf, 'Position', [200, 100, size_scale, size_scale * size_ratio]);
 
-t = tiledlayout(2, 4);
+t = tiledlayout(2, 5);
 t.TileSpacing = 'compact';
 t.Padding = 'compact';
 
 for j = 1:2
     col_idx = 1;
 
-    for i = expl_depths
+    for i = [expl_depths 0]
 
         sp_idx = 4 * (j - 1) + col_idx;
 
-        nexttile
-        hold on
+        ax = nexttile;
 
+        if i == 0
+            continue
+        end
+
+        hold on
         plotDepths(caba_data{j}.depths{i}, caba_opts)
+        plotDepths(aba_data{j}.depths{i}, aba_opts)
+        
         plotDepths(pin_fd_data{j}.depths{i}, pin_fd_opts)
 
         if j == 2
@@ -95,25 +102,31 @@ for j = 1:2
             plotDepths(pin_cd5_data{j}.depths{i}, pin_cd5_opts)
         end
 
-        plotDepths(aba_data{j}.depths{i}, aba_opts)
+        
         plotDepths(pin_aba_data{j}.depths{i}, pin_aba_opts)
 
         % Format applied to all subplots
         grid on
         set(gca, 'XScale', 'linear')
-        % set(gca, 'YScale', 'log')
+        set(gca, 'YScale', 'log')
         set(gca, 'Fontsize', font_size)
         set(gca, 'TickLabelInterpreter', 'latex')
         set(gca, 'XLim', axes(col_idx, 1:2))
-        ax = gca;
-        % ax.YAxis.Exponent = 5;
-        % ax.YRuler.ExponentMode = 'manual';
-        text(1.15, 0.95 * get(gca).YLim(2), ['Loop Type = ', data_names{j}], text_opts)
+        set(gca, 'YLim', axes(col_idx, 3:4))
+        % ax = gca;
+        if strcmp(data_type, 'Instruction')
+            ax.YAxis.Exponent = 5;
+            ax.YRuler.ExponentMode = 'manual';
+        end
+        ax.YAxis.Exponent = -2;
+        ax.YRuler.ExponentMode = 'manual';
+        % text(1.15, 0.98 * get(gca).YLim(2), ['Loop Type: ', data_names{j}], text_opts)
+        text(1.15, 0.96 * get(gca).YLim(2), ['Tree Depth ($d_t$) = ', num2str(i)], text_opts)
 
         % Format Top row
-        if j == 1
-            title(['Tree Depth ($d_t$) = ', num2str(i)], 'Interpreter', 'latex')
-        end
+        % if j == 1
+        %     title(['Tree Depth ($d_t$) = ', num2str(i)], 'Interpreter', 'latex')
+        % end
 
         % Format Bottom row
         if j == 2
@@ -123,28 +136,38 @@ for j = 1:2
         % Format Bottom Right
         if (col_idx == 4 && j == 2)
             l = legend();
-            l.Location = 'Best';
+            % l.Location = 'Best';
             l.Interpreter = 'latex';
-            l.Orientation = 'horizontal';
-            l.IconColumnWidth = 80.;
-            l.Position = [0.0423,0.491,0.921,0.0218];
-            l.FontSize = 8.0;
+            % l.Orientation = 'horizontal';
+            % l.IconColumnWidth = 80.;
+            l.Position = [0.771161583432518,0.430801897780805,0.223156643931198,0.164939341238279];
+            l.FontSize = 10.5;
+        end
+
+        % Format Left column
+        if col_idx == 1
+
+            if strcmp(data_type, 'Instruction')
+                ylabel('Instruction Count', 'Interpreter', 'latex')
+            else
+                ylabel('Computation Time (ms)', 'Interpreter', 'latex')
+            end
+
         end
 
         col_idx = col_idx + 1;
 
     end
 
-    % Format Left column
-    if col_idx == 1
-        ylabel('Instruction Count', 'Interpreter', 'latex')
-    end
-
     col_idx = col_idx + 1;
 
 end
 
-saveas(gcf, '../figures/ParallelChains.png')
+% Delete the two axes on the right to make space for the legend
+delete(t.Children(1))
+delete(t.Children(6))
+
+saveas(gcf, ['../figures/', data_type, 'ParallelChains.png'])
 
 %% Helpers
 function plotDepths(data, opts)
--- Benchmarking/plotting/plottingOptions.m
+++ Benchmarking/plotting/plottingOptions.m
@@ -1,55 +1,77 @@
 load('custom_colors.mat')
-green1 = [0.1, 0.8, 0.4]; % A lighter, minty green
-green2 = [0.2, 0.6, 0.2]; % A medium, natural green
-green3 = [0.0, 0.4, 0.0]; % A dark forest green
 
-% symbols = {'o--', 's--', 'd--', '^--', '*--', 'p--', 'h--', 'x--', '+--'};
-symbols = {'o--', 'o--', 'o--', 'o--', 'o--', 'o--', 'o--', 'o--', 'o--'};
+caba_color = subdued_red;
+aba_color  = [255, 180, 66] / 255;
+pin_fd_color  = [0.1, 0.4, 0.7];       % Steel blue
+pin_cd1_color = [0.6, 0.9, 0.6];       % Mint green
+pin_cd2_color = [0.3, 0.7, 0.3];       % Medium green
+pin_cd5_color = [0.1, 0.5, 0.1];       % Darker green
+pin_aba_color = [0.7, 0.5, 1.0];       % Light violet
 
-marker_size = 8;
+marker_size = 6;
 line_width = 2.0;
 font_size = 14;
+
+grbda_marker = 'o';
+grbda_line = '-';
+
+pin_marker = '^';
+pin_line = '--';
 
 plot_opts.MarkerSize = marker_size;
 plot_opts.LineWidth = line_width;
 
 text_opts.Interpreter = 'latex';
-text_opts.FontSize = font_size;
+text_opts.FontSize = 12.;
 text_opts.Color = 'k';
 text_opts.VerticalAlignment = 'top';
 text_opts.EdgeColor = 'none';
 
 caba_opts = plot_opts;
-caba_opts.Color = subdued_red;
+caba_opts.Color = caba_color;
 caba_opts.MarkerFaceColor = caba_opts.Color;
-caba_opts.DisplayName = 'Constraint-Embedding ABA (Our Implementation)';
+caba_opts.Marker = grbda_marker;
+caba_opts.LineStyle = grbda_line;
+caba_opts.DisplayName = 'grbda::constraintEmbeddingABA()';
 
 pin_fd_opts = plot_opts;
-pin_fd_opts.Color = subdued_blue;
+pin_fd_opts.Color = pin_fd_color;
 pin_fd_opts.MarkerFaceColor = pin_fd_opts.Color;
-pin_fd_opts.DisplayName = 'Cholesky (Pinocchio)';
+pin_fd_opts.Marker = pin_marker;
+pin_fd_opts.LineStyle = pin_line;
+pin_fd_opts.DisplayName = 'pinocchio::forwardDynamics()';
 
 pin_cd1_opts = plot_opts;
-pin_cd1_opts.Color = [0.1, 0.8, 0.4]; % A lighter, minty green
+pin_cd1_opts.Color = pin_cd1_color;
 pin_cd1_opts.MarkerFaceColor = pin_cd1_opts.Color;
-pin_cd1_opts.DisplayName = 'Proximal and Sparse, 1 iter (Pinocchio)';
+pin_cd1_opts.Marker = pin_marker;
+pin_cd1_opts.LineStyle = pin_line;
+pin_cd1_opts.DisplayName = 'pinocchio::constraintDynamics(), 1 iter';
 
 pin_cd2_opts = plot_opts;
-pin_cd2_opts.Color = [0.2, 0.6, 0.2]; % A medium, natural green
+pin_cd2_opts.Color = pin_cd2_color;
 pin_cd2_opts.MarkerFaceColor = pin_cd2_opts.Color;
-pin_cd2_opts.DisplayName = 'Proximal and Sparse, 2 iter (Pinocchio)';
+pin_cd2_opts.Marker = pin_marker;
+pin_cd2_opts.LineStyle = pin_line;
+pin_cd2_opts.DisplayName = 'pinocchio::constraintDynamics(), 2 iter';
 
 pin_cd5_opts = plot_opts;
-pin_cd5_opts.Color = [0.0, 0.4, 0.0]; % A dark forest green
+pin_cd5_opts.Color = pin_cd5_color;
 pin_cd5_opts.MarkerFaceColor = pin_cd5_opts.Color;
-pin_cd5_opts.DisplayName = 'Proximal and Sparse, 5 iter (Pinocchio)';
+pin_cd5_opts.Marker = pin_marker;
+pin_cd5_opts.LineStyle = pin_line;
+pin_cd5_opts.DisplayName = 'pinocchio::constraintDynamics(), 5 iter';
 
 aba_opts = plot_opts;
-aba_opts.Color  = [255, 141, 161] / 255; % A light pink 
+aba_opts.Color  = aba_color;
 aba_opts.MarkerFaceColor = aba_opts.Color;
-aba_opts.DisplayName = 'ABA (Our Implementation)';
+aba_opts.Marker = grbda_marker;
+aba_opts.LineStyle = grbda_line;
+aba_opts.DisplayName = 'grbda::aba()';
 
 pin_aba_opts = plot_opts;
-pin_aba_opts.Color = [222, 65, 255] / 255; % A light magenta
+pin_aba_opts.Color = pin_aba_color;
 pin_aba_opts.MarkerFaceColor = pin_aba_opts.Color;
-pin_aba_opts.DisplayName = 'ABA (Pinocchio)';
+pin_aba_opts.Marker = pin_marker;
+pin_aba_opts.LineStyle = pin_line;
+pin_aba_opts.DisplayName = 'pinocchio::aba()';
